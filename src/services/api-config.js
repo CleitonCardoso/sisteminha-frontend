2ecafe11d0cac2b8f97245d27597e2956f008d42
Cleiton Cardoso <cleitoncardoso_joy@hotmail.com>
2018-03-20 23:53:27 -0300

    fix backend url

--- src/services/api-config.js
+++ src/services/api-config.js
@@ -6,7 +6,7 @@ console.log(hostname)
 
 if (hostname === 'localhost') {
   backendHost = 'http://localhost:8080'
-} else if (hostname === 'https://radiant-shelf-54291.herokuapp.com') {
+} else if (hostname === 'radiant-shelf-54291.herokuapp.com') {
   backendHost = 'https://secure-lake-82403.herokuapp.com'
 }
 
